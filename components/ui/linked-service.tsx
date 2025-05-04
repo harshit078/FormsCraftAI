ae1d10e1431cc4279049b00892a2ecfa0b50f359
harshit078 <harshitsingh467@gmail.com>
2025-05-05 01:28:34 +0530

    Added fix

---
+++ components/ui/linked-service.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React, { type SVGProps } from "react";
+import { GradientHeading } from "@/components/ui/gradient-heading";
+import { LogoCarousel } from "@/components/ui/logo-carousel";
+
+function AppleIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+<svg className='w-24 h-24' fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M16 8.016A8.522 8.522 0 008.016 16h-.032A8.521 8.521 0 000 8.016v-.032A8.521 8.521 0 007.984 0h.032A8.522 8.522 0 0016 7.984v.032z" fill="url(#prefix__paint0_radial_980_20147)"/><defs><radialGradient id="prefix__paint0_radial_980_20147" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="matrix(16.1326 5.4553 -43.70045 129.2322 1.588 6.503)"><stop offset=".067" stopColor="#9168C0"/><stop offset=".343" stopColor="#5684D1"/><stop offset=".672" stopColor="#1BA1E3"/></radialGradient></defs></svg>
+  );
+}
+
+
+function BMWIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+<svg className="w-24 h-24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 242424 333334" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd"><defs><mask id="c"><linearGradient id="a" gradientUnits="userSpaceOnUse" x1="200291" y1="91174.4" x2="200291" y2="176107"><stop offset="0" stopOpacity=".02" stopColor="#fff"/><stop offset="1" stopOpacity=".2" stopColor="#fff"/></linearGradient><path fill="url(#a)" d="M158007 84111h84568v99059h-84568z"/></mask><mask id="e"><radialGradient id="b" gradientUnits="userSpaceOnUse" cx="0" cy="0" r="0" fx="0" fy="0"><stop offset="0" stopOpacity="0" stopColor="#fff"/><stop offset="1" stopOpacity=".098" stopColor="#fff"/></radialGradient><path fill="url(#b)" d="M-150-150h242724v333634H-150z"/></mask><radialGradient id="f" gradientUnits="userSpaceOnUse" cx="9696.85" cy="10000" r="166667" fx="9696.85" fy="10000"><stop offset="0" stopColor="#fff"/><stop offset="1" stopColor="#fff"/></radialGradient><linearGradient id="d" gradientUnits="userSpaceOnUse" x1="200291" y1="94137" x2="200291" y2="173145"><stop offset="0" stopColor="#311a91"/><stop offset="1" stopColor="#311a91"/></linearGradient></defs><g fillRule="nonzero"><path d="M151516 0H22726C10228 0 0 10228 0 22727v287879c0 12494 10228 22728 22726 22728h196971c12494 0 22728-10233 22728-22728V90909l-53037-37880L151516 1z" fill="#673ab6"/><path d="M98482 257569h83333v-15144H98482v15144zm0-90904v15151h83333v-15151H98482zm-18937 7578c0 6268-5094 11363-11365 11363-6269 0-11363-5096-11363-11363 0-6270 5094-11365 11363-11365 6270 0 11365 5094 11365 11365zm0 37872c0 6272-5094 11365-11365 11365-6269 0-11363-5093-11363-11365 0-6266 5094-11363 11363-11363 6270 0 11365 5097 11365 11363zm0 37881c0 6273-5094 11365-11365 11365-6269 0-11363-5092-11363-11365 0-6272 5094-11363 11363-11363 6270 0 11365 5091 11365 11363zm18937-30299h83333v-15155H98482v15155z" fill="#f0f0f0"/><path mask="url(#c)" fill="url(#d)" d="M158158 84261l84266 84242V90909z"/><path d="M151516 0v68181c0 12557 10167 22728 22726 22728h68182L151516 0z" fill="#b39cdb"/><path d="M22726 0C10228 0 0 10228 0 22727v1893C0 12122 10228 1894 22726 1894h128790V1H22726z" fill="#fff" fillOpacity=".2"/><path d="M219697 331433H22726C10228 331433 0 321209 0 308705v1900c0 12494 10228 22728 22726 22728h196971c12494 0 22728-10233 22728-22728v-1900c0 12504-10233 22728-22728 22728z" fill="#311a91" fillOpacity=".2"/><path d="M174243 90909c-12559 0-22726-10171-22726-22728v1893c0 12556 10167 22728 22726 22728h68182v-1893h-68182z" fill="#311a91" fillOpacity=".102"/><path d="M151516 0H22726C10228 0 0 10228 0 22727v287879c0 12494 10228 22728 22726 22728h196971c12494 0 22728-10233 22728-22728V90909L151517 0z" mask="url(#e)" fill="url(#f)"/></g></svg>
+  );
+}
+
+
+function VercelIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+<svg className="w-24 h-24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 326667 333333" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd"><path d="M326667 170370c0-13704-1112-23704-3518-34074H166667v61851h91851c-1851 15371-11851 38519-34074 54074l-311 2071 49476 38329 3428 342c31481-29074 49630-71852 49630-122593m0 0z" fill="#4285f4"/><path d="M166667 333333c44999 0 82776-14815 110370-40370l-52593-40742c-14074 9815-32963 16667-57777 16667-44074 0-81481-29073-94816-69258l-1954 166-51447 39815-673 1870c27407 54444 83704 91852 148890 91852z" fill="#34a853"/><path d="M71851 199630c-3518-10370-5555-21482-5555-32963 0-11482 2036-22593 5370-32963l-93-2209-52091-40455-1704 811C6482 114444 1 139814 1 166666s6482 52221 17777 74814l54074-41851m0 0z" fill="#fbbc04"/><path d="M166667 64444c31296 0 52406 13519 64444 24816l47037-45926C249260 16482 211666 1 166667 1 101481 1 45185 37408 17777 91852l53889 41853c13520-40185 50927-69260 95001-69260m0 0z" fill="#ea4335"/></svg>
+  );
+}
+
+
+const TypeScriptIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 256 256"
+    width={256}
+    height={256}
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    {...props}
+  >
+    <path
+      d="M20 0h216c11.046 0 20 8.954 20 20v216c0 11.046-8.954 20-20 20H20c-11.046 0-20-8.954-20-20V20C0 8.954 8.954 0 20 0Z"
+      fill="#3178C6"
+    />
+    <path
+      d="M150.518 200.475v27.62c4.492 2.302 9.805 4.028 15.938 5.179 6.133 1.151 12.597 1.726 19.393 1.726 6.622 0 12.914-.633 18.874-1.899 5.96-1.266 11.187-3.352 15.678-6.257 4.492-2.906 8.048-6.704 10.669-11.394 2.62-4.689 3.93-10.486 3.93-17.391 0-5.006-.749-9.394-2.246-13.163a30.748 30.748 0 0 0-6.479-10.055c-2.821-2.935-6.205-5.567-10.149-7.898-3.945-2.33-8.394-4.531-13.347-6.602-3.628-1.497-6.881-2.949-9.761-4.359-2.879-1.41-5.327-2.848-7.342-4.316-2.016-1.467-3.571-3.021-4.665-4.661-1.094-1.64-1.641-3.495-1.641-5.567 0-1.899.489-3.61 1.468-5.135s2.362-2.834 4.147-3.927c1.785-1.094 3.973-1.942 6.565-2.547 2.591-.604 5.471-.906 8.638-.906 2.304 0 4.737.173 7.299.518 2.563.345 5.14.877 7.732 1.597a53.669 53.669 0 0 1 7.558 2.719 41.7 41.7 0 0 1 6.781 3.797v-25.807c-4.204-1.611-8.797-2.805-13.778-3.582-4.981-.777-10.697-1.165-17.147-1.165-6.565 0-12.784.705-18.658 2.115-5.874 1.409-11.043 3.61-15.506 6.602-4.463 2.993-7.99 6.805-10.582 11.437-2.591 4.632-3.887 10.17-3.887 16.615 0 8.228 2.375 15.248 7.127 21.06 4.751 5.811 11.963 10.731 21.638 14.759a291.458 291.458 0 0 1 10.625 4.575c3.283 1.496 6.119 3.049 8.509 4.66 2.39 1.611 4.276 3.366 5.658 5.265 1.382 1.899 2.073 4.057 2.073 6.474a9.901 9.901 0 0 1-1.296 4.963c-.863 1.524-2.174 2.848-3.93 3.97-1.756 1.122-3.945 1.999-6.565 2.632-2.62.633-5.687.95-9.2.95-5.989 0-11.92-1.05-17.794-3.151-5.875-2.1-11.317-5.25-16.327-9.451Zm-46.036-68.733H140V109H41v22.742h35.345V233h28.137V131.742Z"
+      fill="#FFF"
+    />
+  </svg>
+);
+
+const TailwindCSSIcon = (props: SVGProps<SVGSVGElement>) => (
+  <img
+  src="/surveymonkey.svg"
+  alt="Next.js"
+  className="w-36 h-36"
+/>
+);
+
+const NextjsIcon = (props: SVGProps<SVGSVGElement>) => (
+  <img
+  src="/typeform.svg"
+  alt="Next.js"
+  className="w-36 h-36"
+/>
+);
+
+// Массив с логотипами
+const allLogos = [
+  { name: "Apple", id: 1, img: AppleIcon },
+  { name: "Vercel", id: 2, img: VercelIcon },
+  { name: "BMW", id: 3, img: BMWIcon },
+  { name: "Nextjs", id: 4, img: NextjsIcon },
+  { name: "Tailwind", id: 5, img: TailwindCSSIcon },
+  { name: "Typescript", id: 6, img: TypeScriptIcon },
+];
+
+export function LogoCarouselDemo() {
+  return (
+    <div className="space-y-8  py-24">
+      <div className="mx-auto flex w-full max-w-screen-lg flex-col items-center space-y-20">
+        <div className="text-center">
+          <GradientHeading variant="secondary">
+            All your favorites in
+          </GradientHeading>
+            <GradientHeading size="xxl"> One Place</GradientHeading>
+        </div>
+        <LogoCarousel columnCount={3} logos={allLogos} /> 
+      </div>
+    </div>
+  );
+}
+
+export default LogoCarouselDemo;
